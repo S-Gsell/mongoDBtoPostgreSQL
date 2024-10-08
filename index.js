58ced4b9019a17fa3a2aa032dea1850eecc611a9
Steven Gsell <s3068106@students.fscj.edu>
2024-10-08 14:36:55 -0400

    updated connections and error throwing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import {connectToMongo, disconnectFromMongo, getCursor} from "./mongoConnection.js";
 import { connectToPSQL, disconnectFromPSQL, idExists } from "./postgresqlConnection.js";
+import * as fs from "node:fs";
 
 
 try{
@@ -7,28 +8,40 @@ try{
     await connectToMongo();
     await connectToPSQL();
 
-    console.log(await idExists(253920));
-    console.log(await idExists(67));
-
-    //iterate through mongoDB
     const cursor = getCursor();
     let game;
-    let testCount = 0;
-    while(game = await cursor.next()){
-        for(let i = 0; i < game.categories.length; i++){
-            if(await idExists(game.steam_appid)){
-                console.log(`game_id: ${game.steam_appid} | category_id: ${game.categories[i].id}`);
-            }   
-        };
-        console.log("");
-        testCount++;
+    //let testCount = 0;
 
-        if(testCount >= 10)
-            break;
+    //write initial csv value
+    fs.writeFile("output.csv", "game_game_id, categories_category_id\n", (err)=>{
+        if(err)
+            console.log("Error with initial CSV write");
+    });
+    
+    //iterate through mongoDB
+    while(game = await cursor.next()){
+        //verify mongoDB data is in postgreSQL
+        if(game.categories && await idExists(game.steam_appid)){
+            //iterate through present categories
+            for(let i = 0; i < game.categories.length; i++){
+                //format csv content
+                const content = `${game.steam_appid}, ${game.categories[i].id}\n`;
+                //write to output file
+                await fs.writeFile("output.csv", content, {flag: "a"}, (err) => {
+                    if(err){
+                        console.error(err);
+                    }
+                });  
+            };
+            //console.log("");
+        }
+        //testCount++;
+
+        //if(testCount >= 100) break;
     }
 
 }catch(e){
-    console.error(e);
+    throw e;
 }finally{
     await disconnectFromMongo();
     await disconnectFromPSQL();
